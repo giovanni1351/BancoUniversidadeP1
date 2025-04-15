9396ff23585c0f8374c560030aef8894619e4e2e
HENRIQUE FINATTI S B TREBBI <unifhtrebbi@feinet.fei.edu.br>
2025-04-15 20:30:02 -0300

    .

--- Querys.sql
+++ Querys.sql
@@ -2,20 +2,34 @@
 ----------------------------------------------- | Querys Obrigatórias | -----------------------------------------------
 /*
 1) Mostre todo o histórico escolar de um aluno que teve reprovação em uma disciplina, retornando inclusive a reprovação em um semestre e a aprovação no semestre seguinte
-    SELECT 
-        b.nome as nome_aluno,
-        c.nome as nome_disciplina,
-        a.semestre_ano,
-        e.nota,
-        e.status
-    FROM disciplinasalunos a
-    LEFT JOIN alunos b on a.id_aluno = b.id
-    LEFT JOIN disciplinas c on a.id_disciplina = c.id
-    LEFT JOIN aluno_historico_escolar d on a.id_aluno = d.id_aluno
-    LEFT JOIN historico_escolar e on d.id_historico_escolar = e.id
-    WHERE 1 = 1
-        and b.nome = 'Rafael Moura'
-        and e.status = 'Trancado' -- quando Givas fizer alteracao trocar por 'Reprovado'
+
+;with cte as(
+    select
+        a.id as aluno
+        ,d.id as disciplina
+        ,count(distinct c.status) as contagem
+    from alunos a
+    inner join aluno_historico_escolar b on a.id = b.id_aluno
+    inner join historico_escolar c on c.id = b.id_historico_escolar
+    inner join disciplinas d on d.id = c.id_disciplina
+    where 1 = 1
+        and a.nome = 'Alexandre da Paz'
+    group by a.id, d.id
+    )
+        
+    select
+    distinct
+        a.nome as Nome_Aluno
+        ,d.nome as Nome_Disciplina
+    ,c.status
+    from alunos a
+    inner join aluno_historico_escolar b on a.id = b.id_aluno
+    inner join historico_escolar c on c.id = b.id_historico_escolar
+    inner join disciplinas d on d.id = c.id_disciplina
+    inner join cte e on a.id = e.aluno
+    where 1 = 1
+    and e.contagem = 2
+    order by 1, 2
 */
 /*
 2) Mostre todos os TCCs orientados por um professor junto com os nomes dos alunos que fizeram o projeto;
@@ -31,6 +45,29 @@
     LEFT JOIN alunos c on a.id = c.id_tcc
     WHERE 1 = 1
         and b.nome = 'Eloá Cassiano'
+*/
+/*
+3) Mostre a matriz curicular de pelo menos 2 cursos diferentes que possuem disciplinas em comum 
+(e.g., Ciência da Computação e Ciência de Dados). Este exercício deve ser dividido em 2 queries sendo uma para cada curso
+    with cte as (
+        select 
+        a.*,
+        b.nome as nome_curso,
+        b.codigo,
+        c.nome as nome_disciplina,
+        row_number() over (partition by c.id order by b.id) as dp
+        from matriz_curricular a
+        LEFT JOIN curso b on a.id_curso = b.id
+        LEFT JOIN disciplinas c on a.id_disciplina = c.id
+    )
+
+    select 
+        * 
+    from cte 
+    where 1 = 1
+        and dp > 1
+    order by 
+        id_disciplina
 */
 /*
 4) Para um determinado aluno, mostre os códigos e nomes das diciplinas já cursadas junto com os nomes dos professores que lecionaram a disciplina para o aluno;
